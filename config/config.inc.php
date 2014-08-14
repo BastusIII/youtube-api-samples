e9b9b721a87eae5ca048a2d6c8e0ee1cb3f824f8
Sebastian Stumpf <sstumpf@zeros.ones.de>
2014-08-14 18:20:16 +0200

    added neutral configs

---
+++ config/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+$config = array ();
+
+/*
+ * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}> Please ensure that you have enabled the YouTube Data API for your project.
+ */
+$config ['DEVELOPER_KEY'] = '';
+
+/*
+ * You can acquire an OAuth 2.0 client ID and client secret from the {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}> For more information about using OAuth 2.0 to access Google APIs, please see: <https://developers.google.com/youtube/v3/guides/authentication> Please ensure that you have enabled the YouTube Data API for your project.
+ */
+$config ['OAUTH2_CLIENT_ID'] = '';
+$config ['OAUTH2_CLIENT_SECRET'] = '';
